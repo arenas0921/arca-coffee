e48c0e1398dee03ef756e943c6f193d2bb543624
arenas0921 <arenas09216671@gmail.com>
2026-08-08 17:57:57 -0500

    Fix Mirador import path

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,7 +2,7 @@ import Hero from "./components/Hero";
 import FeaturedProducts from "../../sections/FeaturedProducts";
 import About from "../../sections/About";
 import ExperiencesPreview from "../../sections/ExperiencesPreview";
-import Mirador from "../../sections/Mirador";
+import Mirador from "../../sections/mirador";
 
 function Home() {
   return (
